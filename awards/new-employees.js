8822f27151f4e37884db7abe4b2fa733fda64bde
Dustin <dustin@proqsolutions.com>
2016-06-14 09:31:27 -0700

    Updated some awards

--- awards/new-employees.js
+++ awards/new-employees.js
@@ -3,7 +3,7 @@ var awards = [
 		uniqueID:		'training-wheels',
 		name:			'No More Training Wheels',
 		iconImage:		':bicyclist:',
-		description:	'It\'s been four weeks, I think you\'ve got the hang of this!',
+		description:	'It\'s 4 weeks at Pro Q!',
 		requiredData:	['dates'], 
 		getLimit:		1,
 		getLimitPer:	'user',
@@ -21,6 +21,32 @@ var awards = [
 
 			//Check if we've past the mark! If so, award it :)
 			if((new Date()) > four_weeks_later) {
+				return Promise.resolve(true);
+			} else {
+				return Promise.resolve(false);
+			}
+		}
+	},
+	{
+		uniqueID:		'welcome',
+		name:			'Liftoff',
+		iconImage:		':rocket:',
+		description:	'Welcome to Pro Q!',
+		requiredData:	['dates'],
+		getLimit:		1,
+		getLimitPer:	'user',
+		getLimitTime:	'ever',
+		checkCallback: 	function(dataGetter, user) {
+			//Check if dates are defined
+			if(!user.dates.hiredate) {
+				return Promise.resolve(false);
+			}
+
+			//Get the Date of users hiredate this year
+			var hire_date = new Date(user.dates.hiredate);
+
+			//Check if today is the user's hiredate
+			if((new Date()).toDateString() == hire_date.toDateString()) {
 				return Promise.resolve(true);
 			} else {
 				return Promise.resolve(false);
