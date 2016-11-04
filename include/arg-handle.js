2cbe285a7a0ef4a9fca80c8301698cbb44b6c92b
Dustin Woods <dustin@proqsolutions.com>
2016-11-04 15:59:37 -0700

    ARg handler abstracted and running

--- include/arg-handle.js
+++ include/arg-handle.js
@@ -8,42 +8,65 @@
 var minimist = require('minimist'),
 	_ = require('lodash');
 
-class something {
+class arg_definition {
+	constructor(options) {
+
+		//Set default properties
+		this.short = '';
+		this.long = '';
+		this.description = '';
+		this.validate = undefined;
+
+		//Set object properties from options parameter
+		_.each(_.keys(this), k => this[k] = this[k] || options[k]);
+
+	}
+}
+
+class command_definition {
+	constructor(options) {
+
+		//Set default properties
+		this.command = '';
+		this.description = '';
+		this.callback = undefined;
+		this.arg_definitions = [];
+
+		//Add arg definitions using add_argument_definition
+		_.each(options['arg_definitions'] || [], k => this.add_argument_definition(k));
+		delete options['arg_definitions'];
+
+		//Set object properties from options parameter
+		_.each(_.keys(this), k => this[k] = this[k] || options[k]);
+
+	}
+
+	//Adds argument definition
+	add_argument_definition(arg) {
+		this.arg_definitions.push(arg);
+	}
+
+	//Calls a command's callback with arguments
+	run_command(args, context) {
+		this.callback.apply(context, [args]);
+	}
 
 }
-var arg_definitions = [
-	{
-		short: 'v',
-		long: 'verbose',
-		default_value: false,
-		description: 'Show lots of useful goodies in the console.',
-		callback: function() {
 
-		}
-	},
-	{
-		short: 'h',
-		long: 'help',
-		description: 'Displays a list of options and commands for Metric Platform.',
-		callback: function() {
-			console.log(arg_definitions);
-		}
-	}
-];
-
+//Parses arguments by cross checking with argument definitions
 class argv_parser {
 	constructor(arg_definitions, _args) {
 		this.arg_definitions = arg_definitions;
 		this.args = [];
 
-		this.args = _args;
-		this.args = this.long_keys_to_short(this.args);
+		this.args = this.base_args = minimist(_args.slice(2), {default: this.default_args_map});
+		this.args = this.short_keys_to_long();
 	}
 
 	//Expands longhand options to shorthand
-	long_keys_to_short() {
-		return _.mapKeys(args,
-					(arg, k) => _.defaultTo(_.find(this.arg_definitions, {long: k}), {short: k}).short
+	short_keys_to_long() {
+		return _.mapKeys(this.args,
+					(arg, k) => _.defaultTo(_.find(this.arg_definitions, {short: k}), {long: k}).long
 				);
 	}
 
@@ -58,28 +81,48 @@ class argv_parser {
 	//Returns keys of unknown args (if any)
 	get unknown_args() {
 		return _.keys(
-					_.pickBy(args,
+					_.pickBy(this.base_args,
 						(arg, k) => _.isUndefined(_.find(this.arg_definitions, {short: k})) && _.isUndefined(_.find(this.arg_definitions, {long: k}))
 					)
 				).filter(arg => arg !== '_');
 	}
 }
 
-//
-//function validate_
+function run_args(commands, argv) {
 
+	this.commands = commands;
 
-var default_args = get_default_args_map(arg_definitions);
+	//Parse and run
+	var pargv = minimist(argv.slice(2), {});
 
-var args = minimist(process.argv.slice(2), {default: default_args})
+	var command = pargv['_'];
+	if(command.length > 1) {
+		//TODO: show help and syntax
+		console.log('Unknown command: ', command);
+		return;
+	} else if(command.length == 0) {
+		//Set default command
+	} else {
+		command = command[0];
+	}
 
-//Check if unknown args were passed, display message and move on
-if((unknown_args = get_unknown_args(args, arg_definitions)).length) {
-	console.log('Unknown options (will be ignored): ', unknown_args);
-	args = _.omit(args, unknown_args);
+	var o_command = _.find(commands, cmd => cmd.command == command);
+
+	if(typeof o_command === 'undefined') {
+		console.log('Unknown command: ', command);
+		return;
+	}
+
+	parsed_args = new argv_parser(o_command.arg_definitions, argv);
+
+	return o_command.run_command(parsed_args.args, this);
+
 }
 
-//Convert long arg keys to short sytax
-args = long_keys_to_short(args, arg_definitions);
 
-console.log(args);
+
+module.exports = {
+	run_args: run_args,
+	arg_definition: arg_definition,
+	command_definition: command_definition
+};
