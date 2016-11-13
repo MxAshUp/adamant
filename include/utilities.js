ebac00a29cc289fd278f4168600fd992402c96e9
Dustin Woods <pyrozzcom@gmail.com>
2016-11-12 19:57:31 -0800

    Moved loop service

--- include/utilities.js
+++ include/utilities.js
@@ -1,6 +1,7 @@
 var fs = require('fs'),
     path = require('path');
 
+
 module.exports = {
 	getPluginsDirectories: function() {
 		srcpath = 'plugins';
@@ -11,28 +12,6 @@ module.exports = {
 		});
 	},
 
-	//Runs fn continuously until condition_fn returns a reject
-	promiseLoop: function(fn, condition_fn) {
-
-		return new Promise((resolve,reject) => {
-			let loop = function() {
-				condition_fn()
-				.catch(() => {
-					resolve();
-					return Promise.reject();
-				})
-				.then(() => {
-					return fn().catch((e) => {
-						reject(e);
-						return Promise.reject();
-					})
-				})
-				.then(loop);
-			};
-
-			loop();
-		});
-	}
 }
 
 
