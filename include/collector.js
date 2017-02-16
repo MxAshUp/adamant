9def142ff2c701312200a101956bc5d7cf0fb7e9
Dustin Woods <dustin@proqsolutions.com>
2017-02-15 17:38:42 -0800

    Refactored collector: propery class pattern

--- include/collector.js
+++ include/collector.js
@@ -1,18 +1,63 @@
-var vsprintf = require("sprintf-js").vsprintf,
+let vsprintf = require("sprintf-js").vsprintf,
 	mongoose = require('./mongoose-utilities'),
 	EventEmitter = require('events'),
 	_ = require('lodash');
 
 
-/**
- * Abstract data collector class
- * @param {object} init_properties - An object of properties to initialize this class with
- * @param {object} args - An object of run args
- */
-var Collector = function(init_properties, args) {
 
-	//Scope it!
-	var self = this;
+class Collector {
+
+	/**
+	 * Creates an instance of Collector.
+	 * 
+	 * @param {object} init_properties - An object of properties to initialize this class with
+	 * @param {object} args - An object of run args
+	 * 
+	 * @memberOf Collector
+	 */
+	constructor(init_properties, args) {
+		
+		// Below stuff would go into the constructor
+		this.default_args = {};
+		this.run_attempts_limit = 5;
+		this.mseconds_between_run_attempts = 500;
+		this.min_mseconds_between_runs = 0;
+		this.model_schema = {};
+		this.model_id_key = '';
+		this.model_name = '';
+		this.version = '';
+		this.plugin_name = '';
+		this.last_run = 0;
+
+		//Set object properties from args
+		for(let i in init_properties) {
+			this[i] = init_properties[i];
+		}
+
+		//Set non-settable properties
+
+		//Set run args
+		args = args ? args : {};
+
+		//Merges args with default args
+		this.args = this.default_args;
+		for (let attrname in args) { this.args[attrname] = args[attrname]; }
+
+		//Set some initial variables
+		this.initialize_flag = false; //If true, initialize will execute before run
+		this.run_attempts = 0; //Count of failed run attempts
+		this.last_run_start = 0; //Timestamp of last run
+		this.stop_flag = false; //Set to true to indicate we need to stop running
+
+		//Registers the model if needed
+		if(!mongoose.modelExists(this.model_name)) {
+			mongoose.createModel(this.model_name, this.model_schema);
+		}
+
+		//Get model
+		this.model = mongoose.getModel(this.model_name);
+	
+	}
 
 
   /**
@@ -20,7 +65,7 @@ var Collector = function(init_properties, args) {
    * @param  {object} args
    * @return {Promise}
    */
-	self.initialize = function(_args) {	};
+	initialize(_args) {	}
 
 
   /**
@@ -28,9 +73,9 @@ var Collector = function(init_properties, args) {
    * @param  {object} args
    * @return {Promise}
    */
-	self.prepare = function(_args) {
+	prepare(_args) {
 		return;
-	};
+	}
 
 
 	/**
@@ -39,9 +84,9 @@ var Collector = function(init_properties, args) {
 	 * @param  {object} _args
 	 * @return {Promise}
 	 */
-	self.collect = function(prepared_data, _args) {
+	collect(prepared_data, _args) {
 		return;
-	};
+	}
 
 
 	/**
@@ -50,71 +95,29 @@ var Collector = function(init_properties, args) {
 	 * @param  {object} _args
 	 * @return {Promise}
 	 */
-	self.garbage = function(prepared_data, _args) {
+	garbage(prepared_data, _args) {
 		return;
-	};
-
-
-
-	// Below stuff would go into the constructor
-	self.default_args = {};
-	self.run_attempts_limit = 5;
-	self.mseconds_between_run_attempts = 500;
-	self.min_mseconds_between_runs = 0;
-	self.model_schema = {};
-	self.model_id_key = '';
-	self.model_name = '';
-	self.version = '';
-	self.plugin_name = '';
-	self.last_run = 0;
-
-	//Set object properties from args
-	for(var i in init_properties) {
-		self[i] = init_properties[i];
 	}
-
-	//Set non-settable properties
-
-	//Set run args
-	args = args ? args : {};
-
-	//Merges args with default args
-	self.args = self.default_args;
-	for (var attrname in args) { self.args[attrname] = args[attrname]; }
-
-	//Set some initial variables
-	self.initialize_flag = false; //If true, initialize will execute before run
-	self.run_attempts = 0; //Count of failed run attempts
-	self.last_run_start = 0; //Timestamp of last run
-	self.stop_flag = false; //Set to true to indicate we need to stop running
-
-	//Registers the model if needed
-	if(!mongoose.modelExists(self.model_name)) {
-		mongoose.createModel(self.model_name, self.model_schema);
-	}
-
-	//Get model
-	self.model = mongoose.getModel(self.model_name);
 
 
 	/**
 	 * Run through the collector functions (initialize, prepare, collect, garbage)
 	 * @return {Promise} Resolves when single run done, rejects when max retries reached from failure
 	 */
-	self.run = function() {
-		var prepared_data;
+	run() {
+		let prepared_data;
 
 		// reset stop flag
-		self.stop_flag = false;
+		this.stop_flag = false;
 
 		// Update last run date
-		self.last_run_start = Date.now();
+		this.last_run_start = Date.now();
 
 		// Begin the run promise chain
-		return (self.initialize_flag ? Promise.resolve() : Promise.reject('Not Initialized'))
+		return (this.initialize_flag ? Promise.resolve() : Promise.reject('Not Initialized'))
 			// If not initialized, then try to initialize
 			.catch(() => {
-				return self.initialize.call(self,self.args)
+				return this.initialize.call(this,this.args)
 				// Reformat possible error
 				.catch((err) => {
 					return Promise.reject('Error initializing: '+err);
@@ -122,17 +125,17 @@ var Collector = function(init_properties, args) {
 			})
 			// Maybe abort if stop_flag enabled
 			.then(() => {
-				return self.stop_flag ? Promise.reject('Stop initiated.') : Promise.resolve();
+				return this.stop_flag ? Promise.reject('Stop initiated.') : Promise.resolve();
 			})
-			// Maybe delay to ensure at least self.min_mseconds_between_runs has passed
+			// Maybe delay to ensure at least this.min_mseconds_between_runs has passed
 			.then(() => {
 				return new Promise((resolve,reject) => {
-					setTimeout(resolve, Math.max(0, Date.now() - self.last_run_start - self.min_mseconds_between_runs));
+					setTimeout(resolve, Math.max(0, Date.now() - this.last_run_start - this.min_mseconds_between_runs));
 				});
 			})
 			// Prepare to collect and remove data
 			.then(() => {
-				return self.prepare.call(self,self.args);
+				return this.prepare.call(this,this.args);
 			})
 			// Data is prepared
 			.then((res) => {
@@ -141,39 +144,42 @@ var Collector = function(init_properties, args) {
 			})
 			// Collect data and insert it
 			.then(() => {
-				return self._collect_then_insert.call(self,prepared_data,self.args)
+				return this._collect_then_insert.call(this,prepared_data,this.args)
 			})
 			// If an error happens inserting data, we won't retry
 			.catch((err) => {
-				self.run_attempts = self.run_attempts_limit;
+				this.run_attempts = this.run_attempts_limit;
 				return Promise.reject(err);
 			})
 			// Remove docs that may need to be removed
 			.then(() => {
-				return self._garbage_then_remove.call(self,prepared_data,self.args)
+				return this._garbage_then_remove.call(this,prepared_data,this.args)
 			})
 			// collect is success, cleanup and return data
 			.then((data) => {
-				self.run_attempts = 0;
-				self.initialize_flag = true; //Set initialize_flag to resolved
+				this.run_attempts = 0;
+				this.initialize_flag = true; //Set initialize_flag to resolved
 				return data;
 			})
 			// If any error occurs during sync, we need to initialize again next time around
 			.catch((err) => {
-				self.initialize_flag = false;
+				this.initialize_flag = false;
 				return Promise.reject(err);
 			})
 			// If any errors occured during collect or initialize, it's caught here and maybe retried
 			// _maybe_retry will throw an error if max attempts reached, then it has to be caught outside this function
-			.catch(self._maybe_retry);
+			.catch(this._maybe_retry);
 	}
 
 
 	/**
 	 * Sets stop flag to initiate a stop
+	 * 
+	 * @todo return a Promise indicating when stop is finished
+	 * @memberOf Collector
 	 */
-	self.stop = function() {
-		self.stop_flag = true;
+	stop() {
+		this.stop_flag = true;
 	}
 
 
@@ -183,8 +189,8 @@ var Collector = function(init_properties, args) {
 	 * @param  {object} args
 	 * @return {Promise}
 	 */
-	self._collect_then_insert = function(data, args) {
-		return self._apply_func_to_func(self.collect, [data, args] , self._insert_data)
+	_collect_then_insert(data, args) {
+		return this._apply_func_to_func(this.collect, [data, args] , this._insert_data)
 		.catch((err) => {
 			//If an error occured, format the error more specifically
 			return Promise.reject('Error collecting doc: '+err);
@@ -197,18 +203,18 @@ var Collector = function(init_properties, args) {
 	 * @param  {object} data_row
 	 * @return {Promise} Promise resolves when success or rejects when error 
 	 */
-	self._insert_data = function(data_row) {
-		return self.model.count(data_row)
+	_insert_data(data_row) {
+		return this.model.count(data_row)
 		.then((res) => {
 			if(res > 0) {
 				//Move along, nothing to update
 				return Promise.resolve();
 			} else {
 				//Update time!
-				var find = {};
-				find[self.model_id_key] = data_row[self.model_id_key];
+				const find = {};
+				find[this.model_id_key] = data_row[this.model_id_key];
 
-				return self.model.findOneAndUpdate(find, data_row, {
+				return this.model.findOneAndUpdate(find, data_row, {
 					upsert:true,
 					setDefaultsOnInsert:true,
 				}).then((oldDoc) => {
@@ -223,9 +229,9 @@ var Collector = function(init_properties, args) {
 		})		
 		.then((is_inserted_row) => {
 			if(is_inserted_row === true) {
-				self.emit('create', data_row); //Execute create event function
+				this.emit('create', data_row); //Execute create event function
 			} else if(is_inserted_row === false) {
-				self.emit('update', data_row); //Execute create event function
+				this.emit('update', data_row); //Execute create event function
 			}
 		})
 		//Catch event handler error
@@ -242,9 +248,9 @@ var Collector = function(init_properties, args) {
 	 * @param  {object} args
 	 * @return {Promise}
 	 */
-	self._garbage_then_remove = function(data, args) {
+	_garbage_then_remove(data, args) {
 
-		return self._apply_func_to_func(self.garbage , [data, args] , self._remove_data)
+		return this._apply_func_to_func(this.garbage , [data, args] , this._remove_data)
 		.catch((err) => {
 			//If an error occured, format the error more specifically
 			return Promise.reject('Error removing doc: '+err);
@@ -257,15 +263,15 @@ var Collector = function(init_properties, args) {
 	 * @param  {object} lookup - Mongoose Lookup
 	 * @return {Promise}
 	 */
-	self._remove_data = function(lookup) {
+	_remove_data(lookup) {
 		if(typeof lookup !== 'object') {
 			throw new Error('Invalid Mongoose lookup.');
 		}
 		//Find doc by lookup and remove it
-		return self.model.findOneAndRemove(lookup).then(function(res) {
+		return this.model.findOneAndRemove(lookup).then(function(res) {
 			//Res is defined if something was found and deleted
 			if(typeof res !== 'undefined') {
-				self.emit('remove', res); //Execute create event function
+				this.emit('remove', res); //Execute create event function
 			}
 			return Promise.resolve(typeof res !== 'undefined');
 		});
@@ -279,30 +285,30 @@ var Collector = function(init_properties, args) {
 	 * @param  {function} func2 - Second function to run, passing in the results from the first function
 	 * @return {Promise} Promise when func2 is done
 	 */
-	self._apply_func_to_func = function(func1, args1, func2) {
-		var is_generator = (func1.constructor.name === 'GeneratorFunction');
+	_apply_func_to_func(func1, args1, func2) {
+		const is_generator = (func1.constructor.name === 'GeneratorFunction');
 		args1 = Array.isArray(args1) ? args1 : [args1];
 		if(is_generator) {
 			// If func1 is a generator (These are cool, see here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/function*)
 			// Loop through yields of func1, create array of promises, one for each yield
 			// Final promise resolves when all promises resolve
-			var promises = [];
-			for(var item of func1.apply(self, args1)) {
+			const promises = [];
+			for(let item of func1.apply(this, args1)) {
 				promises.push(
 					// Pass results to func2, make sure it's a promise
 					Promise.resolve(item).then((res) => {
-						return func2.apply(self, Array.isArray(res) ? res : [res]);
+						return func2.apply(this, Array.isArray(res) ? res : [res]);
 					})
 				);
 			}
 			return Promise.all(promises);
 		} else {
 			// Promisfy, because func1 could return an array
-			return Promise.resolve(func1.apply(self, args1)).then((res_array) => {
+			return Promise.resolve(func1.apply(this, args1)).then((res_array) => {
 				return Promise.all(_.map(res_array, (item) => {
 					// Pass results to func2, make sure it's a promise
 					return Promise.resolve(item).then((res) => {
-						return func2.apply(self, Array.isArray(res) ? res : [res]);
+						return func2.apply(this, Array.isArray(res) ? res : [res]);
 					});
 				}));
 			});
@@ -312,28 +318,28 @@ var Collector = function(init_properties, args) {
 
   /**
    * Execute on this.run() failure, handle retry attempts
-   * @param  {string} err_a
+   * @param  {string} err_a - Error that initiated a retry
    * @return {Promise}
    */
-	self._maybe_retry = function(err_a) {
+	_maybe_retry(err_a) {
 		//Create error message
-		err_a = vsprintf('Attempt %d/%d: %s', [self.run_attempts,self.run_attempts_limit,err_a]);
+		err_a = vsprintf('Attempt %d/%d: %s', [this.run_attempts,this.run_attempts_limit,err_a]);
 
 		//Check if we've reached our failure limit, and make sure stop flag wasn't set
-		if(self.run_attempts < self.run_attempts_limit && self.stop_flag) {
+		if(this.run_attempts < this.run_attempts_limit && this.stop_flag) {
 			//Increment attempt
-			self.run_attempts++;
+			this.run_attempts++;
 			//return a promise containing a timout of a retry
 			return new Promise(function(resolve,reject) {
 				setTimeout(() => {
 					//Try to run again
-					self.run().catch((err_b) => {
+					this.run().catch((err_b) => {
 						err_b = err_b + "\n" + err_a; //Concatenate errors with a new line
 						reject(err_b);
 					}).then((res) => {
 						resolve(res); //Resolved! yay!
 					});
-				}, self.mseconds_between_run_attempts);
+				}, this.mseconds_between_run_attempts);
 			});
 		}
 
