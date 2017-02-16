e3ebc946688fed59a7d53edcd4285f813103286f
Dustin Woods <dustin@proqsolutions.com>
2017-02-15 17:48:02 -0800

    Refactored plugin: proper class

--- include/plugin.js
+++ include/plugin.js
@@ -1,25 +1,32 @@
 /* This is an abstract plugin */
 
-var Plugin = function(args) {
+class Plugin {
 
-	//Scope it!
-	var self = this;
+	/**
+	 * Creates an instance of Plugin.
+	 * 
+	 * @param {object} args
+	 * 
+	 * @memberOf Plugin
+	 */
+	constructor(args) {
+		this.collectors = [];
+		this.enabled = false;
+		this.name = '';
+
+		//Set object properties from args
+		for(var i in args) {
+			this[i] = args[i];
+		}
+	}
 
 	//Settable properties
-	self.on_load = function(_args) {
+	on_load(_args) {
 		//Maybe we need somethign to run when plugin is loaded
 	};
-	self.on_unload = function(_args) {
+	on_unload(_args) {
 		//Maybe we need somethign to run when plugin is unloaded
 	};
-	self.collectors = [];
-	self.enabled = false;
-	self.name = '';
-
-	//Set object properties from args
-	for(var i in args) {
-		self[i] = args[i];
-	}
 
 }
 
