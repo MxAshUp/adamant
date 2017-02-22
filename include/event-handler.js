6c16268c2af767ec2226ae1e2d0ac894c68ef448
Dustin Woods <dustin@proqsolutions.com>
2017-02-22 15:57:56 -0800

    Tabs to spaces, minor adds

--- include/event-handler.js
+++ include/event-handler.js
@@ -1,18 +1,55 @@
+const mongoose = require('./mongoose-utilities'),
+_ = require('lodash');
+//CollectorInitializeError = require('./errors').CollectorInitializeError,
+
 class EventHandler {
 
-    /**
-     * Creates an instance of EventHandler.
-     * 
-     * @param {string} event - Event to listen for
-     * @param {function} callback - Function to run when event is triggered
-     * @param {function} redact_callback - Function to run when event is undone
-     * 
-     * @memberOf EventHandler
-     */
-    constructor(event, callback, redact_callback) {
+  /**
+  * Creates an instance of EventHandler.
+  *
+  *
+  * @memberOf EventHandler
+  */
+  constructor(config, args) {
 
-    }
+    //Default object properties
+    const defaults = {
+      default_args: {},
+      event_name: '',
+      supports_revert: true,
+      version: '',
+      plugin_name: ''
+    };
 
+    // Merge config and assign properties to this
+    Object.assign(this, defaults, config);
 
+    // Merges args with default args
+    this.args = this.default_args;
+    Object.assign(this.args, args);
+
+  }
+
+  /**
+  * Method that reverts actions performed in dispatch
+  *
+  * @param {any} data - Data returned from dispatch event
+  *
+  * @memberOf EventHandler
+  */
+  revert(data) {
+
+  }
+
+  /**
+  * Method that performs action when event is dispatched
+  *
+  * @param {any} data
+  *
+  * @memberOf EventHandler
+  */
+  dispatch(data) {
+
+  }
 
 }
