045896f1ac2bbf046bb57cdacc79a7bbb480fdbf
Josh Buchea <verctrl@joshbuchea.com>
2017-03-01 14:42:10 -0800

    Add one test to PluginLoader “Initialize Collector Service”

--- test/plugin-loader-test.js
+++ test/plugin-loader-test.js
@@ -50,3 +50,43 @@ describe('Plugin Loader', function() {
   });
 
 });
+
+describe('Initialize Collector Service', function() {
+
+  const plugin_loader = new PluginLoader();
+  const plugin_dirs = utilities.getPluginsDirectories();
+
+  // load plugins
+  _.forEach( plugin_dirs, (plugin_path) => { plugin_loader.load_plugin(plugin_path.path, _config); } );
+
+  const collector_configs = [
+  	// {
+  	// 	plugin_name: 'Toggl',
+  	// 	model_name: 'toggl_timeEntry',
+  	// 	version: '1.0',
+  	// 	config: {
+  	// 		apiToken:'771a871d9670b874655a25e20391640f'
+  	// 	}
+  	// },
+  	{
+  		plugin_name: 'TimeClock',
+  		model_name: 'timeclock_timeEntry',
+  		version: '1.0',
+  		config: {
+  			days_back_to_sync: 1,
+  			url:'http://192.168.1.29/',
+  			user:'admin',
+  			password:'FVnZaHD8HyCe'
+  		}
+  	},
+  ];
+  const collector_config = collector_configs[0];
+
+  const service = plugin_loader.initialize_collector_service(collector_config);
+  // service.start();
+
+  it('Should return instance of Loop Service', function () {
+    expect(service).to.be.instanceOf(LoopService);
+  });
+
+});
