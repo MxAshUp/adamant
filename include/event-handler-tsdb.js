849a49224d6b8a708c2202add1e2f1923c965d47
Josh Buchea <verctrl@joshbuchea.com>
2017-03-15 16:34:51 -0700

    Add comments about data structure for tsdb events

--- include/event-handler-tsdb.js
+++ include/event-handler-tsdb.js
@@ -1,4 +1,4 @@
-module.exports = () => {
+module.exports = (influx) => {
 	return [
 		{
       default_args: {},
@@ -7,8 +7,32 @@ module.exports = () => {
       version: '0.1',
       plugin_name: '_core',
       dispatch: (data, event_id) => {
+
         // @todo - write data to tsdb
         // @todo - return data maybe necessary for revert()
+
+				// InfluxDB records consist of a measurement, tags, fields, and a timestamp.
+
+				// - measurement (name/slug): `cpu_load_short`
+				// - timestamp: `1434055562000000000`
+				// - tags: `host=server01,region=us-west`
+				// - fields:
+					// - value (value is a field): `0.64`
+
+				// {
+				// 	measurement: 'cpu_load_short',
+				// 	timestamp: 1434055562000000000,
+				// 	tags: {
+				// 		'host': 'server01',
+				// 		'region': 'us-west',
+				// 	},
+				// 	fields: {
+				// 		'value': 0.64,
+				// 	},
+				// }
+
+				return data;
+
       },
       revert: (data, event_id) => {
         // @todo - remove data from tsdb
