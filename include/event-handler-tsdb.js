19e85dfb7e620c85ace2b77a72e98c10f46da7cc
Josh Buchea <verctrl@joshbuchea.com>
2017-03-22 13:00:20 -0700

    Make prettier :lipstick:

--- include/event-handler-tsdb.js
+++ include/event-handler-tsdb.js
@@ -1,47 +1,47 @@
-module.exports = (influx) => {
-	return [
-		{
+module.exports = influx => {
+  return [
+    {
       default_args: {},
       event_name: 'metric.write',
       supports_revert: true,
       version: '0.1',
       plugin_name: '_core',
       dispatch: (data, event_id) => {
-
         // @todo - write data to tsdb
         // @todo - return data maybe necessary for revert()
 
-				// InfluxDB records consist of a measurement, tags, fields, and a timestamp:
+        // InfluxDB records consist of a measurement, tags, fields, and a timestamp:
 
-				// {
-				// 	measurement: 'cpu_load_short',
-				// 	timestamp: 1434055562000000000,
-				// 	tags: {
-				// 		'host': 'server01',
-				// 		'region': 'us-west',
-				// 	},
-				// 	fields: {
-				// 		'value': 0.64,
-				// 	},
-				// }
+        // {
+        // 	measurement: 'cpu_load_short',
+        // 	timestamp: 1434055562000000000,
+        // 	tags: {
+        // 		'host': 'server01',
+        // 		'region': 'us-west',
+        // 	},
+        // 	fields: {
+        // 		'value': 0.64,
+        // 	},
+        // }
 
-				influx.writePoints([
-		      // {
-		      //   measurement: 'response_times',
-		      //   tags: { host: os.hostname() },
-		      //   fields: { duration, path: req.path },
-		      // },
-					data,
-		    ]).catch(err => {
-		      console.error(`Error saving data to InfluxDB! ${err.stack}`)
-		    });
+        influx
+          .writePoints([
+            // {
+            //   measurement: 'response_times',
+            //   tags: { host: os.hostname() },
+            //   fields: { duration, path: req.path },
+            // },
+            data,
+          ])
+          .catch(err => {
+            console.error(`Error saving data to InfluxDB! ${err.stack}`);
+          });
 
-				return data;
-
+        return data;
       },
       revert: (data, event_id) => {
         // @todo - remove data from tsdb
-      }
-    }
+      },
+    },
   ];
 };
