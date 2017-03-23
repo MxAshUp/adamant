d73f95c5987cac85d29af608c271527793c774b9
Dustin Woods <dustin@proqsolutions.com>
2017-03-22 17:25:49 -0700

    Updated debug plugin to match new factory pattern

--- plugins/debug/event-handlers.js
+++ plugins/debug/event-handlers.js
@@ -1,36 +1,34 @@
 const chalk = require('chalk');
 
-module.exports = (_config) => {
-	return [
-		{
-      default_args: {},
-      event_name: 'toggl_timeEntry.create',
-      supports_revert: false,
-      version: '0.1',
-      plugin_name: '_core',
-      dispatch: (data, event_id) => {
-         console.log(`${chalk.bgCyan('Toggle TimeEntry')} ${chalk.green('created')}: ${chalk.grey(JSON.stringify(data))}`);
-      }
-    },
-		{
-      default_args: {},
-      event_name: 'toggl_timeEntry.update',
-      supports_revert: false,
-      version: '0.1',
-      plugin_name: '_core',
-      dispatch: (data, event_id) => {
-        console.log(`${chalk.bgCyan('Toggle TimeEntry')} ${chalk.yellow('updated')}: ${chalk.grey(JSON.stringify(data))}`);
-      }
-    },
-		{
-      default_args: {},
-      event_name: 'toggl_timeEntry.remove',
-      supports_revert: false,
-      version: '0.1',
-      plugin_name: '_core',
-      dispatch: (data, event_id) => {
-        console.log(`${chalk.bgCyan('Toggle TimeEntry')} ${chalk.red('removed')}: ${chalk.grey(JSON.stringify(data))}`);
-      }
+module.exports = [
+  {
+    default_args: {},
+    event_name: 'toggl.time_entry.create',
+    supports_revert: false,
+    version: '0.1',
+    plugin_name: '_core',
+    dispatch: (data, event_id) => {
+        console.log(`${chalk.bgCyan('Toggle TimeEntry')} ${chalk.green('created')}: ${chalk.grey(JSON.stringify(data))}`);
     }
-  ];
-};
+  },
+  {
+    default_args: {},
+    event_name: 'toggl.time_entry.update',
+    supports_revert: false,
+    version: '0.1',
+    plugin_name: '_core',
+    dispatch: (data, event_id) => {
+      console.log(`${chalk.bgCyan('Toggle TimeEntry')} ${chalk.yellow('updated')}: ${chalk.grey(JSON.stringify(data))}`);
+    }
+  },
+  {
+    default_args: {},
+    event_name: 'toggl.time_entry.remove',
+    supports_revert: false,
+    version: '0.1',
+    plugin_name: '_core',
+    dispatch: (data, event_id) => {
+      console.log(`${chalk.bgCyan('Toggle TimeEntry')} ${chalk.red('removed')}: ${chalk.grey(JSON.stringify(data))}`);
+    }
+  }
+];
--- plugins/debug/index.js
+++ plugins/debug/index.js
@@ -1,12 +1,7 @@
 /**
  * @todo load plugin stuff here
  */
-module.exports = function(_config) {
-
-	/*Plugin initializing done here...*/
-
-	return {
-		name: 'Debug Tools',
-		event_handlers: require('./event-handlers.js')(_config)
-	};
+module.exports = {
+	name: 'Debug Tools',
+	event_handlers: require('./event-handlers.js')
 };
