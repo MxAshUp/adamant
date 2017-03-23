ac61c8ed1bae2bc5b0d3a8333d62325b13f99640
Dustin Woods <dustin@proqsolutions.com>
2017-03-23 12:05:36 -0700

    Update sync.js

--- sync.js
+++ sync.js
@@ -10,27 +10,46 @@ const app = new App();
 
 app.load_plugins(utilities.getPluginsDirectories());
 
-app.load_plugin_event_handlers();
-
-// app.load_collector({
-// 	plugin_name: 'Toggl',
-// 	collector_name: 'CollectorTimeEntries',
-// 	version: '1.0',
-// 	config: {
-// 		apiToken:'771a871d9670b874655a25e20391640f'
-// 	}
-// });
-
 app.load_collector({
-	plugin_name: 'TimeClock',
-	collector_name: 'CollectorPunches',
+	plugin_name: 'Toggl',
+	collector_name: 'CollectorTimeEntries',
 	version: '1.0',
 	config: {
-		days_back_to_sync: 1,
-		url:'http://192.168.1.29/',
-		user:'admin',
-		password:'FVnZaHD8HyCe'
+		apiToken:'771a871d9670b874655a25e20391640f'
 	}
 });
 
+// app.load_collector({
+// 	plugin_name: 'TimeClock',
+// 	collector_name: 'CollectorPunches',
+// 	version: '1.0',
+// 	config: {
+// 		days_back_to_sync: 1,
+// 		url:'http://192.168.1.29/',
+// 		user:'admin',
+// 		password:'FVnZaHD8HyCe'
+// 	}
+// });
+
+app.load_event_handler({
+	plugin_name: 'Debug Tools',
+	handler_name: 'HandlerConsoleLogger',
+	version: '1.0',
+	config: {
+		event_name: 'toggl.time_entry.update',
+		label_style: 'bgYellow'
+	}
+});
+app.load_event_handler({
+	plugin_name: 'Debug Tools',
+	handler_name: 'HandlerConsoleLogger',
+	version: '1.0',
+	config: {
+		event_name: 'toggl.time_entry.create',
+		label_style: 'bgCyan'
+	}
+});
+// toggl.time_entry.create
+// toggl.time_entry.update
+// toggl.time_entry.remove
 app.init().then(app.run.bind(app));
