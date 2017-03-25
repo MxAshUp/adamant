6d0fc34e636f8b3135dff6699f61061a7cb2b84e
Josh Buchea <verctrl@joshbuchea.com>
2017-03-24 17:39:32 -0700

    Add logic to _maybe_retry

--- include/loop-service.js
+++ include/loop-service.js
@@ -20,8 +20,8 @@ class LoopService extends EventEmitter {
 
     this.retry_attempts = 0;
     this.retry_max_attempts = 2;
-    this.retry_time_between = 0;
-    this.retry_errors = [];
+    this.retry_time_between = 3000;
+    this.retry_errors = ['yourmom'];
     this.retry_errors_to_skip = [];
   }
 
@@ -68,13 +68,27 @@ class LoopService extends EventEmitter {
     console.log('this.retry_errors: ', this.retry_errors);
     console.log('this.retry_errors_to_skip: ', this.retry_errors_to_skip);
 
+    // increment retry attempts
     this.retry_attempts++;
 
+    // max retry attempts reached
     if (this.retry_attempts > this.retry_max_attempts) {
-      return true;
+      console.log('max retry attempts reached!');
+      return false;
     }
 
-    // return false;
+    // error not in errors to catch
+    if (this.retry_errors.indexOf(err) === -1) {
+      console.log('error not in errors to catch!');
+      return false;
+    }
+
+    // error is in errors to skip
+    if (this.retry_errors_to_skip.indexOf(err) > -1) {
+      console.log('error is in errors to skip!');
+      return false;
+    }
+
     return true;
   }
 
@@ -116,6 +130,8 @@ class LoopService extends EventEmitter {
         // Try to call run_callback
         try {
           Promise.resolve(this.run_callback()).catch(reject).then(() => {
+            // reject('yourmom');
+
             // If all went well, let's do it again!
             setImmediate(loopfn);
           });
@@ -132,11 +148,16 @@ class LoopService extends EventEmitter {
     })
       .catch(e => {
         // Turn errors into emitted event
-        this.emit('error', e);
+        // this.emit('error', e);
 
         if (this._maybe_retry(e)) {
-          console.log('_maybe_retry says retry yo!');
-          // this.start();
+          setTimeout(
+            () => {
+              console.log('_maybe_retry says retry!');
+              // this.start();
+            },
+            this.retry_time_between
+          );
         }
       })
       .then(() => {
