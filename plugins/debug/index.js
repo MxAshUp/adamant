87534744c3149fdadd2a96cffcdc3a2d25660ca7
Josh Buchea <verctrl@joshbuchea.com>
2017-05-16 10:44:34 -0700

    Map collector events to socket.io

--- plugins/debug/index.js
+++ plugins/debug/index.js
@@ -15,5 +15,22 @@ module.exports = {
     // });
     // loop services and map events
     // loop collectors and map events
+
+    // console.log('app obj keys: ', Object.keys(app));
+    // console.log('app.collect_services: ', app.collect_services);
+
+    setTimeout(() => {
+      // console.log('app.collect_services: ', app.collect_services);
+
+      for (let collector of app.collect_services) {
+        for (let event_name in collector._events) {
+          collector.on(event_name, () => {
+            app.io.emit(event_name);
+            console.log(`socket.io ${event_name} event emitted`);
+          });
+          console.log(`${collector.name} ${event_name} event mapped`);
+        }
+      }
+    }, 3500);
   },
 };
