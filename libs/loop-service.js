850c0310bf3b2f4c64608114a3d3bca623721480
Josh Buchea <verctrl@joshbuchea.com>
2017-08-08 15:53:42 -0700

    Make prettier :lipstick:

--- libs/loop-service.js
+++ libs/loop-service.js
@@ -59,25 +59,36 @@ class LoopService extends EventEmitter {
 	 * @return {Promise} resolves if can retry, rejects with error otherwise
 	 */
   _maybe_retry(err) {
-
     // We're not allowed to retry
-    if(this.retry_max_attempts === 0) {
+    if (this.retry_max_attempts === 0) {
       return Promise.reject(err);
     }
 
     // max retry attempts reached
     if (this.retry_attempts >= this.retry_max_attempts) {
       this.retry_attempts = 0; // reset
-      return Promise.reject(new Error(`Max retry attempts (${this.retry_max_attempts}) reached.`));
+      return Promise.reject(
+        new Error(`Max retry attempts (${this.retry_max_attempts}) reached.`)
+      );
     }
 
     // if errors_only_retry_on has item(s) AND error not in errors to catch, then bubble it up
-    if (this.errors_only_retry_on.length && err.constructor && err.constructor.name && this.errors_only_retry_on.indexOf(err.constructor.name) === -1) {
+    if (
+      this.errors_only_retry_on.length &&
+      err.constructor &&
+      err.constructor.name &&
+      this.errors_only_retry_on.indexOf(err.constructor.name) === -1
+    ) {
       return Promise.reject(err);
     }
 
     // error is in errors to skip
-    if (this.errors_dont_retry_on.length && err.constructor && err.constructor.name && this.errors_dont_retry_on.indexOf(err.constructor.name) > -1) {
+    if (
+      this.errors_dont_retry_on.length &&
+      err.constructor &&
+      err.constructor.name &&
+      this.errors_dont_retry_on.indexOf(err.constructor.name) > -1
+    ) {
       return Promise.reject(err);
     }
 
@@ -109,7 +120,6 @@ class LoopService extends EventEmitter {
     this.emit('started');
 
     return new Promise((resolve, reject) => {
-
       this.loop_function_resolve_cb = resolve;
       this.loop_function_reject_cb = reject;
 
@@ -121,49 +131,51 @@ class LoopService extends EventEmitter {
         } else {
           // Keep running...
 
-          Promise.resolve().then(this.run_callback) // <--- this is where run_callback is executed
-          .then(() => {
-            // If all went well, let's do it again!
-            this.run_count++; // <--- note this only increments on success
-            this.retry_attempts = 0; // reset retries
+          Promise.resolve()
+            .then(this.run_callback) // <--- this is where run_callback is executed
+            .then(() => {
+              // If all went well, let's do it again!
+              this.run_count++; // <--- note this only increments on success
+              this.retry_attempts = 0; // reset retries
 
-            // Set timeout for next loop_function run
-            this.loop_function_timeout_id = setTimeout(
-              loop_function,
-              this.run_min_time_between
-            );
-          }).catch((e) => {
-            // Catch errors from run_callback
+              // Set timeout for next loop_function run
+              this.loop_function_timeout_id = setTimeout(
+                loop_function,
+                this.run_min_time_between
+              );
+            })
+            .catch(e => {
+              // Catch errors from run_callback
 
-            // Maybe we'll retry loop_function
-            return this._maybe_retry(e)
-              .then((attempt) => {
-                // We're good to retry!
+              // Maybe we'll retry loop_function
+              return this._maybe_retry(e)
+                .then(attempt => {
+                  // We're good to retry!
 
-                // Emit error
-                this.emit('error', e);
+                  // Emit error
+                  this.emit('error', e);
 
-                // Retrying
-                this.emit('retry', attempt);
+                  // Retrying
+                  this.emit('retry', attempt);
 
-                // Set timeout for next loop_function run
-                this.loop_function_timeout_id = setTimeout(
-                  loop_function,
-                  this.retry_time_between
-                );
-              }).catch((er) => {
-                this.emit('error', er);
-                this.loop_function_resolve_cb();
-              });
-          })
-          .catch(this.loop_function_reject_cb); // <-- This only happens for unhandled exceptions
+                  // Set timeout for next loop_function run
+                  this.loop_function_timeout_id = setTimeout(
+                    loop_function,
+                    this.retry_time_between
+                  );
+                })
+                .catch(er => {
+                  this.emit('error', er);
+                  this.loop_function_resolve_cb();
+                });
+            })
+            .catch(this.loop_function_reject_cb); // <-- This only happens for unhandled exceptions
         }
       }).bind(this);
 
       // Start the loop
       setImmediate(loop_function);
-    })
-    .then(() => {
+    }).then(() => {
       this.run_status = false;
       this.stop_on_run = 0;
 
@@ -171,7 +183,6 @@ class LoopService extends EventEmitter {
       this.emit('stopped'); //<-- Note, if error is thrown in handlers of this event, it will need to be caught by the code that executes start()
     });
   }
-
 
   /**
 	 * Initiates the LoopService to stop running.
