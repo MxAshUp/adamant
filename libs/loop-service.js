16ebc1f6a20bc4147b852870c3450028f7669b41
Josh Buchea <verctrl@joshbuchea.com>
2017-08-08 16:23:40 -0700

    Reduce nesting & simplify code comprehension: extract loop_function, store promise in var

--- libs/loop-service.js
+++ libs/loop-service.js
@@ -119,57 +119,57 @@ class LoopService extends EventEmitter {
     // Trigger start event
     this.emit('started');
 
-    return new Promise((resolve, reject) => {
+    // Loop function
+    const loop_function = () => {
+      if (this._should_stop) {
+        // Should Stop is true if we've reach out run limit, or we're instructed to stop
+        return this.loop_function_resolve_cb();
+      }
+
+      Promise.resolve()
+        .then(this.run_callback) // <--- this is where run_callback is executed
+        .then(() => {
+          // If all went well, let's do it again!
+          this.run_count++; // <--- note this only increments on success
+          this.retry_attempts = 0; // reset retries
+
+          // Set timeout for next loop_function run
+          this.loop_function_timeout_id = setTimeout(
+            loop_function,
+            this.run_min_time_between
+          );
+        })
+        .catch(e => {
+          // Catch errors from run_callback
+
+          // Maybe we'll retry loop_function
+          return this._maybe_retry(e)
+            .then(attempt => {
+              // We're good to retry!
+
+              // Emit error
+              this.emit('error', e);
+
+              // Retrying
+              this.emit('retry', attempt);
+
+              // Set timeout for next loop_function run
+              this.loop_function_timeout_id = setTimeout(
+                loop_function,
+                this.retry_time_between
+              );
+            })
+            .catch(er => {
+              this.emit('error', er);
+              this.loop_function_resolve_cb();
+            });
+        })
+        .catch(this.loop_function_reject_cb); // <-- This only happens for unhandled exceptions
+    };
+
+    const promise = new Promise((resolve, reject) => {
       this.loop_function_resolve_cb = resolve;
       this.loop_function_reject_cb = reject;
-
-      // Loop function
-      let loop_function = () => {
-        if (this._should_stop) {
-          // Should Stop is true if we've reach out run limit, or we're instructed to stop
-          return this.loop_function_resolve_cb();
-        }
-
-        Promise.resolve()
-          .then(this.run_callback) // <--- this is where run_callback is executed
-          .then(() => {
-            // If all went well, let's do it again!
-            this.run_count++; // <--- note this only increments on success
-            this.retry_attempts = 0; // reset retries
-
-            // Set timeout for next loop_function run
-            this.loop_function_timeout_id = setTimeout(
-              loop_function,
-              this.run_min_time_between
-            );
-          })
-          .catch(e => {
-            // Catch errors from run_callback
-
-            // Maybe we'll retry loop_function
-            return this._maybe_retry(e)
-              .then(attempt => {
-                // We're good to retry!
-
-                // Emit error
-                this.emit('error', e);
-
-                // Retrying
-                this.emit('retry', attempt);
-
-                // Set timeout for next loop_function run
-                this.loop_function_timeout_id = setTimeout(
-                  loop_function,
-                  this.retry_time_between
-                );
-              })
-              .catch(er => {
-                this.emit('error', er);
-                this.loop_function_resolve_cb();
-              });
-          })
-          .catch(this.loop_function_reject_cb); // <-- This only happens for unhandled exceptions
-      };
 
       // Start the loop
       setImmediate(loop_function);
@@ -180,6 +180,8 @@ class LoopService extends EventEmitter {
       //Emit stopped event
       this.emit('stopped'); //<-- Note, if error is thrown in handlers of this event, it will need to be caught by the code that executes start()
     });
+
+    return promise;
   }
 
   /**
