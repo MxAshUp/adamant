efee48d14221ba19fdd86acd39bb2ea3b01d3a5a
Dustin Woods <dustin@proqsolutions.com>
2017-08-17 16:22:52 -0700

    Fixed #31 - defer removes handler. Re-addresses defer_fn return false

--- libs/utility.js
+++ libs/utility.js
@@ -1,15 +1,24 @@
 const defer_on_event = (event_name, defer_fn, event_emitter) => {
+
+  const handle_fn = () => {
+    Promise.resolve()
+    .then(defer_fn)
+    .then((res) => {
+      if(res) {
+        event_emitter.removeHandler(event_name,handle_fn);
+        resolve();
+      }
+    })
+    .catch((e) => {
+      event_emitter.removeHandler(event_name,handle_fn);
+      reject(e);
+    })
+  };
+
   return new Promise((resolve, reject) => {
-    event_emitter.on(
+    event_emitter.addHandler(
       event_name,
-      defer_fn
-        .then(
-          res =>
-            res
-              ? resolve()
-              : reject(new Error('Event handler defer fn resolved false'))
-        )
-        .catch(e => reject(new Error(e)))
+      handle_fn
     );
   });
 };
