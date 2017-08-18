2b63ad111a5b29c5f563b70e14f218db318ae7dd
Josh Buchea <verctrl@joshbuchea.com>
2017-08-18 10:10:44 -0700

    Fix incorrect method names

--- libs/utility.js
+++ libs/utility.js
@@ -4,18 +4,18 @@ const defer_on_event = (event_name, defer_fn, event_emitter) => {
       .then(defer_fn)
       .then(res => {
         if (res) {
-          event_emitter.removeHandler(event_name, handle_fn);
+          event_emitter.removeListener(event_name, handle_fn);
           resolve();
         }
       })
       .catch(e => {
-        event_emitter.removeHandler(event_name, handle_fn);
+        event_emitter.removeListener(event_name, handle_fn);
         reject(e);
       });
   };
 
   return new Promise((resolve, reject) => {
-    event_emitter.addHandler(event_name, handle_fn);
+    event_emitter.addListener(event_name, handle_fn);
   });
 };
 
