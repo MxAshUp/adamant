83a205c1d024be11bba1d755f85fc5d5547ae079
Dustin Woods <dustin@proqsolutions.com>
2017-09-07 17:19:40 -0700

    Fixed defer_on_event not removing listener

--- libs/utility.js
+++ libs/utility.js
@@ -1,21 +1,28 @@
+/**
+ * Creates a promise that executes defer_fn and resolves if returns truthy. defer_fn is executed only when event_emitter has event_name emitted
+ *
+ * @param {String} event_name
+ * @param {Function} defer_fn
+ * @param {EventEmitter} event_emitter
+ * @returns
+ */
 const defer_on_event = (event_name, defer_fn, event_emitter) => {
-  const handle_fn = (resolve, reject) => {
-    Promise.resolve()
-      .then(defer_fn)
-      .then(res => {
-        if (res) {
-          event_emitter.removeListener(event_name, handle_fn);
-          resolve();
-        }
-      })
-      .catch(e => {
-        event_emitter.removeListener(event_name, handle_fn);
-        reject(e);
-      });
-  };
-
   return new Promise((resolve, reject) => {
-    event_emitter.addListener(event_name, () => handle_fn(resolve, reject));
+    const handle_fn = () => {
+      Promise.resolve()
+        .then(defer_fn)
+        .then(res => {
+          if (res) {
+            event_emitter.removeListener(event_name, handle_fn);
+            resolve();
+          }
+        })
+        .catch(e => {
+          event_emitter.removeListener(event_name, handle_fn);
+          reject(e);
+        });
+    };
+    event_emitter.addListener(event_name, handle_fn);
   });
 };
 
