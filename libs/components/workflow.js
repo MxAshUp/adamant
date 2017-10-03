ad24bdc234cac2e09012a86af74d9e72986c35fd
Dustin Woods <dustin@proqsolutions.com>
2017-10-02 20:36:44 -0700

    Updates to workflow

--- libs/components/workflow.js
+++ libs/components/workflow.js
@@ -1,72 +1,84 @@
 let EventHandler = require('./event-handler');
 let Event = require('../event');
+let workflow_count = 0;
 
 class Workflow extends EventHandler {
-  constructor({
-    event_name,
-    should_handle = '',
-    defer_dispatch = '',
-    load_component_function = null,
-  } = {}) {
 
-    super({
-      event_name: event_name,
-      should_handle: should_handle,
-      defer_dispatch: defer_dispatch,
-      transform_function: null,
-      enqueue_complete_event: true,
-    });
+  /**
+   * Creates an instance of Workflow.
+   * @param - See EventHandler Class for arguments
+   * @memberof Workflow
+   */
+  constructor({workflow_name} = {}) {
+    super(arguments);
 
-    this.load_component = load_component_function;
-
+    this.workflow_name = workflow_name ? workflow_name : Workflow.generate_workflow_name();
     this.event_handler_sequence = [this];
+    workflow_count ++;
+  }
 
+  static generate_workflow_name() {
+    return `workflow_${workflow_count}`;
   }
 
   register_step(handler) {
+
+    if(!this.event_handler_sequence) {
+      this.event_handler_sequence = [this];
+    }
+
     // Get the current step
-    const current_step = this.get_step_length();
+    const current_step = this.get_current_step();
+
+    // Get previous step
+    const previous_step = current_step - 1;
+    const previous_step_handler = this.event_handler_sequence[previous_step];
 
     // Change the handler event name to fit in the workflow
     handler.event_name = this.format_event_name(current_step, handler.event_name);
 
     // Add transitioning handler
-    this._augment_handler_for_transition(previous_handler, current_step - 1);
+    const original_transform_function = previous_step_handler.transform_function;
+    previous_step_handler.transform_function = this._transition_dispatch.bind(this, handler.event_name, original_transform_function);
 
     // Add handler to sequence
     this.event_handler_sequence.push(handler);
+
+    return this;
   }
 
-  _augment_handler_for_transition(handler, step_number) {
-    const original_transform_function = handler.transform_function;
-    handler.transform_function = this._transition_dispatch.bind(this, handler.event_name, original_transform_function);
-  }
+  // _augment_handler_for_transition(previous_handler, current_handler, step_number) {
+  //   const original_transform_function = previous_handler.transform_function;
+  //   previous_handler.transform_function = this._transition_dispatch.bind(this, current_handler.event_name, original_transform_function);
+  // }
 
-  _create_transition_handler(handler, step_number) {
-    const previous_step = step_number - 1;
-    const previous_step_handler = this.event_handler_sequence[previous_step];
-    const previous_step_handler_name = previous_step_handler.event_name;
+  // _create_transition_handler(handler, step_number) {
+  //   const previous_step = step_number - 1;
+  //   const previous_step_handler = this.event_handler_sequence[previous_step];
+  //   const previous_step_handler_name = previous_step_handler.event_name;
 
-    const transition_handler_parameters = {
-      event_name: `${previous_step_handler_name}.complete`,
-      transform_function: this._transition_dispatch.bind(this, handler.event_name)
-    };
+  //   const transition_handler_parameters = {
+  //     event_name: `${previous_step_handler_name}.complete`,
+  //     transform_function: this._transition_dispatch.bind(this, handler.event_name)
+  //   };
 
-    return this.load_component('EventHandler', transition_handler_parameters);
-  }
+  //   return this.load_component('EventHandler', transition_handler_parameters);
+  // }
 
-  _transition_dispatch(event_name, data, original_transform_function) {
+  _transition_dispatch(event_name, original_transform_function, data) {
     const transition_event = new Event(event_name, data);
     this.emit('enqueue_event', transition_event);
     return original_transform_function(data);
   }
 
-  get_step_length() {
+  get_current_step() {
     return this.event_handler_sequence.length;
   }
 
   format_event_name(step_number, event_name) {
-    return `${this.workflow_name}.step_${step_number}.${event_name}`;
+    const name_parts = [this.workflow_name, `step_${step_number}`, event_name];
+    const filtered_name_parts = name_parts.filter(part => part); // Remove parts of name that are empty
+    return filtered_name_parts.join('.'); // Join parts with dot
   }
 }
 
