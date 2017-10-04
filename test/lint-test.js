d9ff4ccecfec49abc65a7b38a2dc276dd356a898
Dustin Woods <dustin@proqsolutions.com>
2017-10-04 13:13:42 -0700

    Improved lint test, removed local-acorn-globals

--- test/lint-test.js
+++ test/lint-test.js
@@ -1,41 +1,44 @@
 const glob = require('glob');
 const fs = require('fs');
-const detect = require('local-acorn-globals');
-const _ = require('lodash');
+const detect = require('acorn-globals');
+const getLineInfo = require('acorn').getLineInfo;
 const path = require('path');
 
-describe('Linting source file', () => {
-  const files_to_test = glob.sync(`${__dirname}/../libs/**/*.js`);
-  // For now, we skip app because it uses console.log
-  const skip_files = ['libs/app.js'];
+// Config
+const paths_to_search = ['libs'];
+const skip_files = ['libs/app.js']; // example: libs/index.js
+const blacklist_globals = ['console'];
 
+// Generate globs and find files
+const glob_paths = paths_to_search.map(path => `${__dirname}/../${path}/**/*.js`);
+const glob_pattern = `{${glob_paths.join(',')}}`;
+const files = glob.sync(glob_pattern);
+const files_to_test = files.filter(file => {
+  return (skip_files.indexOf(path.relative(`${__dirname}/..`, file)) === -1)
+});
+
+describe('Linting source file', () => {
   files_to_test.forEach(source_file => {
     // Get relative file name being parsed
     const print_file = path.relative(`${__dirname}/..`, source_file);
-
-    if(skip_files.indexOf(print_file) !== -1) {
-      return;
-    }
-
     describe(`${print_file}`, () => {
       let scope;
       const file_content = fs.readFileSync(source_file, 'utf8');
       it(`Should be valid JavaScript`, () => {
         scope = detect(file_content, {locations: true});
       });
-      // List of globals not to use
-      const blacklist_globals = ['console'];
 
       // Loop through each
       blacklist_globals.forEach(global_name => {
         it(`Should not use global ${global_name}`, () => {
 
           // Check if global was found in scope
-          const console_scope = _.find(scope, {name: global_name});
-          if(console_scope) {
+          const globals_scope = scope.find(node => node.name === global_name);
+          if(globals_scope) {
             // Create list of {line}:{column} identifiers
-            const locations = console_scope.nodes.map(ref => {
-              return `${print_file}:${ref.loc.start.line}:${ref.loc.start.column}`;
+            const locations = globals_scope.nodes.map(ref => {
+              const line_info = getLineInfo(file_content, ref.start);
+              return `${print_file}:${line_info.line}:${line_info.column}`;
             });
             // Throw error with location description of each
             throw new Error(`'${global_name}' used at: ` + "\n" + locations.join("\n"));
